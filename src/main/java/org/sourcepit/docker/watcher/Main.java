c2a489bcf22f4278af3720787e1ff5c6499a46c8
Bernd Vogt <Bernd.Vogt@bosch-si.com>
2015-08-21 10:49:53 +0200

    Prepared for INST network

--- src/main/java/org/sourcepit/docker/watcher/Main.java
+++ src/main/java/org/sourcepit/docker/watcher/Main.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2015 Bernd Vogt and others.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,8 +41,8 @@ public class Main {
          }
       };
 
-      final String dockerDaemonUri = "http://192.168.56.101:2375";
-      final String consulAgentUri = "http://192.168.56.101:8500";
+      final String dockerDaemonUri = "http://192.168.56.104:2375";
+      final String consulAgentUri = "http://192.168.56.104:8500";
 
       final BlockingQueue<List<JsonObject>> queue = new LinkedBlockingQueue<>();
 
